5b518488c49143875c1ee6669bbaa3c81d80b8bd
inha1234 <wind150804@gmail.com>
2023-06-12 18:37:46 +0900

    initial

---
+++ src/test/java/com/example/section5unit2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.section5unit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
